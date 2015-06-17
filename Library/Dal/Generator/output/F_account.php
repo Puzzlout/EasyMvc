332c31054e33bf10a9abc28ff947e1600a77f973
WebDevJL <webdev.jl@gmail.com>
2015-06-17 10:53:50 +0200

    Issue #1

--- Library/Dal/Generator/output/F_account.php
+++ Library/Dal/Generator/output/F_account.php
@@ -1,18 +1,17 @@
 <?php
-/**** @author Jeremie Litzler* @copyright Copyright (c) 2015* @link * @since Version 1.0.0* @package F_account.php*/
-namespace \Library\Dal\Modules\;F_accountif ( ! defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed');
+/*** @author Jeremie Litzler* @copyright Copyright (c) 2015* @licence * @link https://github.com/WebDevJL/EasyMVC* @since Version 1.0.0* @package F_account*/
+namespace \Library\Dal\Modules;if ( ! defined('__EXECUTION_ACCESS_RESTRICTION__')) { exit('No direct script access allowed'); }
 class F_account extends \Library\Entity {  public     $f_account_id,    $f_account_name,    $f_account_desc,    $f_account_active,    $f_account_visible,    $f_user_id;
-  const     F_ACCOUNT_ID_ERR = 0,    F_ACCOUNT_NAME_ERR = 1,    F_ACCOUNT_DESC_ERR = 2,    F_ACCOUNT_ACTIVE_ERR = 3,    F_ACCOUNT_VISIBLE_ERR = 4,    F_USER_ID_ERR = 5;
-  // SETTERS //  public function setF_account_id($f_account_id) {      $this->f_account_id = $f_account_id;  }
-  public function setF_account_name($f_account_name) {      $this->f_account_name = $f_account_name;  }
-  public function setF_account_desc($f_account_desc) {      $this->f_account_desc = $f_account_desc;  }
-  public function setF_account_active($f_account_active) {      $this->f_account_active = $f_account_active;  }
-  public function setF_account_visible($f_account_visible) {      $this->f_account_visible = $f_account_visible;  }
-  public function setF_user_id($f_user_id) {      $this->f_user_id = $f_user_id;  }
-  // GETTERS //  public function f_account_id() {    return $this->f_account_id;  }
-  public function f_account_name() {    return $this->f_account_name;  }
-  public function f_account_desc() {    return $this->f_account_desc;  }
-  public function f_account_active() {    return $this->f_account_active;  }
-  public function f_account_visible() {    return $this->f_account_visible;  }
-  public function f_user_id() {    return $this->f_user_id;  }
+  /**    * Sets f_account_id if the value passed match the type expected, otherwise throws an exception.  */  public function setF_account_id($f_account_id) {      $this->f_account_id = $f_account_id;  }
+  /**    * Sets f_account_name if the value passed match the type expected, otherwise throws an exception.  */  public function setF_account_name($f_account_name) {      $this->f_account_name = $f_account_name;  }
+  /**    * Sets f_account_desc if the value passed match the type expected, otherwise throws an exception.  */  public function setF_account_desc($f_account_desc) {      $this->f_account_desc = $f_account_desc;  }
+  /**    * Sets f_account_active if the value passed match the type expected, otherwise throws an exception.  */  public function setF_account_active($f_account_active) {      $this->f_account_active = $f_account_active;  }
+  /**    * Sets f_account_visible if the value passed match the type expected, otherwise throws an exception.  */  public function setF_account_visible($f_account_visible) {      $this->f_account_visible = $f_account_visible;  }
+  /**    * Sets f_user_id if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_id($f_user_id) {      $this->f_user_id = $f_user_id;  }
+  /**    * Gets f_account_id.  */  public function F_account_id() {    return $this->f_account_id;  }
+  /**    * Gets f_account_name.  */  public function F_account_name() {    return $this->f_account_name;  }
+  /**    * Gets f_account_desc.  */  public function F_account_desc() {    return $this->f_account_desc;  }
+  /**    * Gets f_account_active.  */  public function F_account_active() {    return $this->f_account_active;  }
+  /**    * Gets f_account_visible.  */  public function F_account_visible() {    return $this->f_account_visible;  }
+  /**    * Gets f_user_id.  */  public function F_user_id() {    return $this->f_user_id;  }
 }
--- Library/Dal/Generator/output/F_user.php
+++ Library/Dal/Generator/output/F_user.php
@@ -1,22 +1,21 @@
 <?php
-/**** @author Jeremie Litzler* @copyright Copyright (c) 2015* @link * @since Version 1.0.0* @package F_user.php*/
-namespace \Library\Dal\Modules\;F_userif ( ! defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed');
+/*** @author Jeremie Litzler* @copyright Copyright (c) 2015* @licence * @link https://github.com/WebDevJL/EasyMVC* @since Version 1.0.0* @package F_user*/
+namespace \Library\Dal\Modules;if ( ! defined('__EXECUTION_ACCESS_RESTRICTION__')) { exit('No direct script access allowed'); }
 class F_user extends \Library\Entity {  public     $f_user_id,    $f_user_login,    $f_user_password,    $f_user_salt,    $f_user_hint,    $f_user_email,    $f_user_role_id,    $f_user_session_id;
-  const     F_USER_ID_ERR = 0,    F_USER_LOGIN_ERR = 1,    F_USER_PASSWORD_ERR = 2,    F_USER_SALT_ERR = 3,    F_USER_HINT_ERR = 4,    F_USER_EMAIL_ERR = 5,    F_USER_ROLE_ID_ERR = 6,    F_USER_SESSION_ID_ERR = 7;
-  // SETTERS //  public function setF_user_id($f_user_id) {      $this->f_user_id = $f_user_id;  }
-  public function setF_user_login($f_user_login) {      $this->f_user_login = $f_user_login;  }
-  public function setF_user_password($f_user_password) {      $this->f_user_password = $f_user_password;  }
-  public function setF_user_salt($f_user_salt) {      $this->f_user_salt = $f_user_salt;  }
-  public function setF_user_hint($f_user_hint) {      $this->f_user_hint = $f_user_hint;  }
-  public function setF_user_email($f_user_email) {      $this->f_user_email = $f_user_email;  }
-  public function setF_user_role_id($f_user_role_id) {      $this->f_user_role_id = $f_user_role_id;  }
-  public function setF_user_session_id($f_user_session_id) {      $this->f_user_session_id = $f_user_session_id;  }
-  // GETTERS //  public function f_user_id() {    return $this->f_user_id;  }
-  public function f_user_login() {    return $this->f_user_login;  }
-  public function f_user_password() {    return $this->f_user_password;  }
-  public function f_user_salt() {    return $this->f_user_salt;  }
-  public function f_user_hint() {    return $this->f_user_hint;  }
-  public function f_user_email() {    return $this->f_user_email;  }
-  public function f_user_role_id() {    return $this->f_user_role_id;  }
-  public function f_user_session_id() {    return $this->f_user_session_id;  }
+  /**    * Sets f_user_id if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_id($f_user_id) {      $this->f_user_id = $f_user_id;  }
+  /**    * Sets f_user_login if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_login($f_user_login) {      $this->f_user_login = $f_user_login;  }
+  /**    * Sets f_user_password if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_password($f_user_password) {      $this->f_user_password = $f_user_password;  }
+  /**    * Sets f_user_salt if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_salt($f_user_salt) {      $this->f_user_salt = $f_user_salt;  }
+  /**    * Sets f_user_hint if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_hint($f_user_hint) {      $this->f_user_hint = $f_user_hint;  }
+  /**    * Sets f_user_email if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_email($f_user_email) {      $this->f_user_email = $f_user_email;  }
+  /**    * Sets f_user_role_id if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_role_id($f_user_role_id) {      $this->f_user_role_id = $f_user_role_id;  }
+  /**    * Sets f_user_session_id if the value passed match the type expected, otherwise throws an exception.  */  public function setF_user_session_id($f_user_session_id) {      $this->f_user_session_id = $f_user_session_id;  }
+  /**    * Gets f_user_id.  */  public function F_user_id() {    return $this->f_user_id;  }
+  /**    * Gets f_user_login.  */  public function F_user_login() {    return $this->f_user_login;  }
+  /**    * Gets f_user_password.  */  public function F_user_password() {    return $this->f_user_password;  }
+  /**    * Gets f_user_salt.  */  public function F_user_salt() {    return $this->f_user_salt;  }
+  /**    * Gets f_user_hint.  */  public function F_user_hint() {    return $this->f_user_hint;  }
+  /**    * Gets f_user_email.  */  public function F_user_email() {    return $this->f_user_email;  }
+  /**    * Gets f_user_role_id.  */  public function F_user_role_id() {    return $this->f_user_role_id;  }
+  /**    * Gets f_user_session_id.  */  public function F_user_session_id() {    return $this->f_user_session_id;  }
 }
