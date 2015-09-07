10ccdf53bd353219a5af9856020d4e5c8ed27056
Jérémie Litzler <webdev.jl@gmail.com>
2015-09-07 23:14:44 +0200

    Issue #23
    Removed comment

--- Library/Core/HttpResponse.php
+++ Library/Core/HttpResponse.php
@@ -31,9 +31,6 @@ class HttpResponse extends ApplicationComponent {
   }
 
   public function send() {
-    // Actuellement, cette ligne a peu de sens dans votre esprit.
-    // Promis, vous saurez vraiment ce qu'elle fait d'ici la fin du chapitre
-    // (bien que je suis sûr que les noms choisis sont assez explicites !).
     if (!$this->app->router->isWsCall) {
       //Return the content to page
       exit($this->page->getGeneratedPage());
