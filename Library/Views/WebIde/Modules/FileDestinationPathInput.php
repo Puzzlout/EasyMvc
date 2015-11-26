e0817e9353adbc78a070ae719e6f5361b67a312b
Jérémie Litzler <webdev.jl@gmail.com>
2015-11-26 07:48:06 +0100

    #104

--- Library/Views/WebIde/Modules/FileDestinationPathInput.php
+++ Library/Views/WebIde/Modules/FileDestinationPathInput.php
@@ -2,8 +2,8 @@
 if (!FrameworkConstants_ExecutionAccessRestriction) {
   exit('No direct script access allowed');
 }
+echo Library\UC\ScriptControl::Init()->ForInternalResource("Web/library/js/webide.solution.folderpaths.js");
 ?>
-<script type="application/javascript" src="<?php echo FrameworkConstants_BaseUrl; ?>Web/library/js/webide.solution.folderpaths.js"></script>
 <div class="form-group">
   <label for="fileDirPath">Type file destination</label>
   <input id="fileDirPath" class="form-control" type="text" placeholder="Starting typing to autocomplete..." />
