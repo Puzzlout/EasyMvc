dd6e92cf37153ad05c411f042de238258a8e1dda
Jérémie Litzler <contact@jeremielitzler.net>
2015-12-04 12:49:34 +0000

    Tweaks database deploy script

--- Database/Scripts/db_init_on_server.sh
+++ Database/Scripts/db_init_on_server.sh
@@ -1,6 +1,10 @@
+#$1 => the mysql user
+#$2 => the folder where db_reset.sql is stored
 echo "Begin database initialization"
-SCRIPTPATH=$(dirname "$SCRIPT")
-echo $SCRIPTPATH
-# mysql -u "webdevjl" -p ""
-mysql .\ /workspace/$SCRIPTPATH/db_reset.sql
-echo "Finished database initialization"
+echo "Folder to db_reset.sql is: "$2
+#echo "Connecting to MySQL with user > "$1
+#echo "Command is: mysql -u "$1
+#mysql -u $1 -p 
+echo "Connected ! Running the database initialization..."
+#mysql -u $1 -p '' .\ $2"db_reset.sql"
+echo "Finished database initialization!"
