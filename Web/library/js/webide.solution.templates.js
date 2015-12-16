86a41f6c74f2fa3f13e5b73e1e6424817f930886
Jérémie Litzler <webdev.jl@gmail.com>
2015-12-16 08:27:19 +0100

    #112
    Solved the complexity added with last commit c31c195

--- Web/library/js/webide.solution.templates.js
+++ Web/library/js/webide.solution.templates.js
@@ -1,14 +1,16 @@
 (function($) {
   var fileType = $("#fileType").find(":selected").attr("data-id");
-  if(!utils.isNullOrEmpty(fileType)) {
-    datacx.post("WebIdeAjax/GetTemplateContents", {"fileType":fileType}).then(function(reply) {
-      if (reply === null || reply.result === 0) {//has an error
-        toastr.error(reply.message);
-        return undefined;
-      } else {//success
-        toastr.success(reply.message);
-      }
-    });
+  if(utils.isNullOrEmpty(fileType)) {
+    throw "fileType is null or empty";
+  }
+  datacx.post("WebIdeAjax/GetTemplateContents", {"fileType":fileType}).then(function(reply) {
+    if (reply === null || reply.result === 0) {//has an error
+      toastr.error(reply.message);
+      return undefined;
+    }
+    //success
+    toastr.success(reply.message);
+  });
   }
 
 })(jQuery);
