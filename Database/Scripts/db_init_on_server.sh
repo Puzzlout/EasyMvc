223910839517095b6e8ecf41dd61ecebaea980fa
Jérémie Litzler <contact@jeremielitzler.net>
2015-12-24 07:19:09 +0000

    Deploy DB script tweaked to display command to run

--- Database/Scripts/db_init_on_server.sh
+++ Database/Scripts/db_init_on_server.sh
@@ -2,9 +2,8 @@
 #$2 => the folder where db_reset.sql is stored
 echo "Begin database initialization"
 echo "Folder to db_reset.sql is: "$2
-#echo "Connecting to MySQL with user > "$1
-#echo "Command is: mysql -u "$1
-#mysql -u $1 -p 
-echo "Connected ! Running the database initialization..."
-#mysql -u $1 -p '' .\ $2"db_reset.sql"
+echo "Connecting to MySQL with user > "$1
+echo "Command is => mysql -u "$1
+echo "Running the database initialization:"
+echo "Command is => mysql -u $1 -p '' .\ $2\"db_reset.sql\""
 echo "Finished database initialization!"
